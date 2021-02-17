4cbcc9333f0c18c270f69b0f282b2607b932bb69
Yashleenk <78870105+Yashleenk@users.noreply.github.com>
2021-02-17 11:21:44 -0600

    It separates odd and even numbers

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = new int[] {
+            54, 91, 23, 90, 4, 12, 43, 56, 49, 10
+         };
+            int[] arr2 = new int[10];
+            int[] arr3 = new int[10];
+            int i, j = 0, k = 0;
+
+            for (i = 0; i < 10; i++)
+            {
+                if (arr1[i] % 2 == 0)
+                {
+                    arr2[j] = arr1[i];
+                    j++;
+                }
+                else
+                {
+                    arr3[k] = arr1[i];
+                    k++;
+                }
+            }
+
+            Console.WriteLine("Even numbers...");
+            for (i = 0; i < j; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+
+            Console.WriteLine("Odd numbers...");
+            for (i = 0; i < k; i++)
+            {
+                Console.WriteLine(arr3[i]);
+            }
+        }
+    }
+}
