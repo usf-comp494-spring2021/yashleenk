2432b64977b77749f08ff47a61276161ff007fe3
Yashleenk <78870105+Yashleenk@users.noreply.github.com>
2021-02-23 18:03:05 -0600

    Final Homework 3

---
+++ HW3/CalcLibTest/xUnitTest1.cs
@@ -0,0 +1,61 @@
+using System;
+using HW3;
+using Xunit;
+
+namespace CalcuLibTest
+{
+    public class xUnitTest1
+    {
+        [Fact]
+        public void TestAdd2and4()
+        {
+            double a = 2;
+            double b = 4;
+            double expected = 6;
+            var calc = new Calc();
+
+            double actual = calc.add(a, b);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TestSubstract10and8()
+        {
+            double a = 10;
+            double b = 8;
+            double expected = 2;
+            var calc = new Calc();
+
+            double actual = calc.sub(a, b);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TestMultiply5and5()
+        {
+            double a = 5;
+            double b = 5;
+            double expected = 25;
+            var calc = new Calc();
+
+            double actual = calc.mult(a, b);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TestDivide10and2()
+        {
+            double a = 10;
+            double b = 2;
+            double expected = 5;
+            var calc = new Calc();
+
+            double actual = calc.divi(a, b);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ HW3/HW3/Calc.cs
@@ -0,0 +1,32 @@
+﻿namespace HW3
+{
+
+    public class Calc
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double divi(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
